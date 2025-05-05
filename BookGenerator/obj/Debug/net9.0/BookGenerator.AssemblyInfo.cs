11e8c47201efe6ee81de87a1d585d5f631062454
Haxinpro <teemu.karne@hotmail.com>
2025-05-05 10:59:16 +0300

    korjaus

--- BookGenerator/obj/Debug/net9.0/BookGenerator.AssemblyInfo.cs
+++ BookGenerator/obj/Debug/net9.0/BookGenerator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd8575ec8c678c88c3357202994a4fdc58f13d70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f99dc7b26e5d99c2da058f90959b228008549c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
